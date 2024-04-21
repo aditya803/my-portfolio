51d12eeb06386a70fbfa40f6d8f18df6842b4e57
Aditya Pandey <adityapan25@gmail.com>
2024-04-21 21:23:37 +0530

    contact form functionality added

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,36 +1,53 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './Contact.css'
 import FacebookIcon from '../../assets/facebook-icon.png'
 import TwitterIcon from '../../assets/twitter.png'
 import YoutubeIcon from '../../assets/youtube.png'
 import InstagramIcon from '../../assets/instagram.png'
-
+import emailjs from '@emailjs/browser';
 
 
 
 const Contact = () => {
-  return (
-    <section id="contactPage">
-        <div className="contact">
-            <h1 className="contactPageTitle">Contact Me</h1>
-            <span className="contactDesc">
-                Please fill out a form to discuss work oppurtunities.
-            </span>
-            <form className="contactForm">
-                <input type="text" className="name" placeholder='Your Name' />
-                <input type="text" className="email" placeholder='Your E-mail' />
-                <textarea name="message" placeholder='Your Message' className='msg' rows="5"></textarea>
-                <button className="submitBtn" type='submit' value='Send'>Submit</button>
-                <div className="links">
-                    <img src={FacebookIcon} alt="FacebookIcon" className="link" />
-                    <img src={TwitterIcon} alt="TwitterIcon" className="link" />
-                    <img src={YoutubeIcon} alt="YoutubeIcon" className="link" />
-                    <img src={InstagramIcon} alt="InstagramIcon" className="link" />
-                </div>
-            </form>
-        </div>
-    </section>
-  )
+    const form = useRef();
+    const sendEmail = (e) => {
+        e.preventDefault();
+        emailjs
+          .sendForm('service_moj9btb', 'template_umsydyn', form.current,'Oqds7uQacnR8tctR_',)
+          .then(
+            (result) => {
+              console.log('SUCCESS!');
+              e.target.reset();
+              alert('Email Sent!');
+            },
+            (error) => {
+              console.log('FAILED...', error.text);
+            },
+          );
+      };
+
+    return (
+        <section id="contactPage">
+            <div className="contact">
+                <h1 className="contactPageTitle">Contact Me</h1>
+                <span className="contactDesc">
+                    Please fill out a form to discuss work oppurtunities.
+                </span>
+                <form className="contactForm" ref={form} onSubmit={sendEmail}>
+                    <input type="text" className="name" placeholder='Your Name' name='your_name'/>
+                    <input type="text" className="email" placeholder='Your E-mail' name='your_email' />
+                    <textarea name="message" placeholder='Your Message'  className='msg' rows="5"></textarea>
+                    <button className="submitBtn" type='submit' value='Send'>Submit</button>
+                    <div className="links">
+                        <img src={FacebookIcon} alt="FacebookIcon" className="link" />
+                        <img src={TwitterIcon} alt="TwitterIcon" className="link" />
+                        <img src={YoutubeIcon} alt="YoutubeIcon" className="link" />
+                        <img src={InstagramIcon} alt="InstagramIcon" className="link" />
+                    </div>
+                </form>
+            </div>
+        </section>
+    )
 }
 
 export default Contact
--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './navbar.css';
 import logo from '../../assets/logo.png';
 import {Link} from 'react-scroll';
 import contactImg from '../../assets/contact.png';
+import menu from '../../assets/menu.png'
 
 const Navbar = () => {
+  const[showmenu, setshowmenu] = useState(false);
   return (
     <nav className="navBar">
         <img src={logo} alt='Logo' className='logo'/>
@@ -14,12 +16,20 @@ const Navbar = () => {
             <Link activeClass= 'active' to='works' spy={true} smooth={true} offset={-100} duration={500} className="desktopMenuListItem">Portfolio</Link>
             <Link activeClass= 'active' to='contactPage' spy={true} smooth={true} offset={-100} duration={500} className="desktopMenuListItem">Clients</Link>
         </div>
-        <button className="desktopMenuBtn" onClick={()=>{
+        {/* <button className="desktopMenuBtn" onClick={()=>{
           document.getElementById('contactPage').scrollIntoView({behavior: 'smooth'});
         }}>
             <img src={contactImg} alt="" className='desktopMenuImg'/>
             Contact Me
-        </button>
+        </button> */}
+        <img src={menu} alt='Menu' className='mobMenu' onClick={()=> setshowmenu(!showmenu)}/>
+        <div className="navMenu" style={{display: showmenu? 'flex':'none'}}>
+            <Link activeClass= 'active' to='intro' spy={true} smooth={true} offset={-100} duration={500} className="listItem" onClick={()=>setshowmenu(false)}>Home</Link>
+            <Link activeClass= 'active' to='skills' spy={true} smooth={true} offset={-100} duration={500} className="listItem" onClick={()=>setshowmenu(false)}>About</Link>
+            <Link activeClass= 'active' to='works' spy={true} smooth={true} offset={-100} duration={500} className="listItem" onClick={()=>setshowmenu(false)}>Portfolio</Link>
+            {/* <Link activeClass= 'active' to='contactPage' spy={true} smooth={true} offset={-100} duration={500} className="listItem" onClick={()=>setshowmenu(false)}>Clients</Link> */}
+            <Link activeClass= 'active' to='contact' spy={true} smooth={true} offset={-100} duration={500} className="listItem" onClick={()=>setshowmenu(false)}>Contact</Link>
+        </div>
     </nav>
   )
 }
