7372705b3aa46ebcfe637c4696de02f0018f6967
Aditya Pandey <adityapan25@gmail.com>
2024-04-21 22:33:41 +0530

    personal customization

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './intro.css';
-import bg from '../../assets/image.png';
+import bg from '../../assets/profile-photo.png';
 import btnimg from '../../assets/hireme.png';
 import { Link } from 'react-scroll';
 
@@ -9,8 +9,8 @@ const Intro = () => {
     <section id="intro">
       <div className="introContent"> 
         <span className="hello">Hello,</span>
-        <span className="introText">I'm <span className="introName">Aditya</span><br/>Developer</span>
-        <p className="introPara">Random Text</p>
+        <span className="introText">I'm <span className="introName">Aditya</span><br/>Software Developer</span>
+        <p className="introPara">Hi, I am a passionate Software Developer with 1.5 years of experience,<br/> specializing in Backend and Flutter app development.<br/> I am probably creating a silly games as we speak.</p>
         <Link><button className="btn"><img src={btnimg} alt='Hire Me' className='btnImg'/>Hire Me</button></Link>
       </div>
       <img src={bg} alt="Profile" className="bg"/>
